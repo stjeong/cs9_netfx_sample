cec4e109b6a22e743137e91d1c9b50c77c983b05
SeongTae Jeong <kevin13@chol.net>
2020-11-11 11:05:16 +0900

    Record 예제 추가

---
+++ Chapter17/예제17.02/Program.cs
@@ -0,0 +1,16 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point p = new(5, 6);
+    }
+}
+
+public record Point
+{
+    public int X;
+    public int Y;
+
+    public Point(int x, int y) { X = x; Y = y; }
+}
